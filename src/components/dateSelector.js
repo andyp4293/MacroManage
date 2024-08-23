335459355df1fb0d72667443fbb13c097e980a7b
andyp4293 <andyp4293@gmail.com>
2024-08-23 10:55:23 -0400

    Created date selector component for weightlog page

--- src/components/dateSelector.js
+++ src/components/dateSelector.js
@@ -4,4 +4,62 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { format, addDays, subDays } from 'date-fns';
 
+function DateSelector(){
+    // state for the selected date
+    const [date, setDate] = useState(new Date()); // starts off with the current date
 
+    // function to go to the previous day
+    const prevDay = () => {
+        setDate(subDays(date, 1)); 
+    };
+
+    // function go to to the next day
+    const nextDay = () => {
+        setDate(addDays(date, 1)); 
+    };
+    
+    return (
+        <Button
+        variant = "contained"
+        sx = {{
+            backgroundColor: '#00c691',
+            display: 'flex', 
+            alignItems: 'center',
+            justifyContent: 'space-between', 
+            borderRadius: '20px', 
+            padding: '10px 20px',
+            minWidth: '250px',
+            '&:hover':{
+                backgroundColor: '#00a67e',
+            }
+        }}
+        >
+
+        <IconButton
+            color = "inherent"
+            onClick = {prevDay}
+            sx = {{
+                padding: '0', 
+                marginRight: '10px'
+            }}
+        >
+            <ArrowBackIosIcon />
+        </IconButton>
+
+        <Typography variant="body1" sx={{ color: 'white' }}>
+        {format(date, 'EEEE, MMM d, yyyy')}
+        </Typography>
+
+        <IconButton
+        color = 'white'
+        onClick={nextDay}
+        sx={{ padding: '0', marginLeft: '10px' }}
+        >
+            <ArrowForwardIosIcon />
+        </IconButton>
+        </Button> 
+    )
+
+};
+
+export default DateSelector;
--- src/pages/Weightlog.js
+++ src/pages/Weightlog.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import DateSelector from '../components/dateSelector'; 
 import '../styles/Weightlog.css';
 
 function WeightLog() {
     return (
-            <div className="weight-log">
-                <div className="date-container">
-                    <label htmlFor="date">Date: </label>
-                    <input type="date" id="date-selector" defaultValue="2024-08-19" />
-                </div>
+            <div>
+                <DateSelector/>
             </div>
     );
 }
