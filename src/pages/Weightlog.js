4d55b80917595fa57ae0fcbcf2f8afae28d58bb4
andyp4293 <andyp4293@gmail.com>
2024-08-25 18:36:59 -0400

    Converting weightlog.css into a css module and changing implemented for weightlog.js

--- src/pages/Weightlog.js
+++ src/pages/Weightlog.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import DateSelector from '../components/dateSelector'; 
-import '../styles/Weightlog.module.css';
+import styles from '../styles/Weightlog.module.css';
 
 function WeightLog() {
     return (
             <div>
-                <div className = 'date-container'>
+                <div className = {styles['date-container']}>
                     <h3>Log Entry For:</h3>
                     <DateSelector/>
                 </div> 
                 <hr></hr>
-                <div className = 'weight-input'> 
+                <div className = {styles['weight-input']}> 
                     <h4>Enter weigh-in</h4>
                     <input // input for the weight log of the day
                         name = 'weight' 
