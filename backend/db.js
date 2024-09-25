875a1cf9acba5fc40aa46fb1d80cbfbc1211f765
andyp4293 <andyp4293@gmail.com>
2024-09-25 18:27:42 -0400

    made env variables for database pool information

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+require('dotenv').config({ path: './.env' });
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    PORT: process.env.DB_PORT,
+});
+
+
+
+module.exports = pool;
