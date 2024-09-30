19e06ab9c7decb13e5c0ad14136278b346b0ea30
andyp4293 <andyp4293@gmail.com>
2024-09-29 21:23:28 -0400

    added dotenv to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config({ path: './.env' });
 const port = process.env.PORT || 5000;
 
 app.use(cors());
