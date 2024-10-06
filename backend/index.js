945c59ab8b627e46d8036564f650972456c4c937
andyp4293 <andyp4293@gmail.com>
2024-10-06 15:25:06 -0400

    changed back to normal http

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,30 @@
 const express = require('express');
-const https = require('https');  // Import the https module
-const fs = require('fs');        // Import the fs module to read the certificate and key
+const app = express();
 const cors = require('cors');
 require('dotenv').config({ path: './.env' });
-
-const app = express();
 const port = process.env.PORT || 5000;
-
-// Load the self-signed SSL certificate and key
-const sslOptions = {
-    key: fs.readFileSync('/etc/ssl/private/selfsigned.key'),
-    cert: fs.readFileSync('/etc/ssl/certs/selfsigned.crt')
-};
 
 app.use(cors());
 app.use(express.json());
 
 const nutritionRoutes = require('./routes/nutrition');
-const mealsRoutes = require('./routes/meals');
+const mealsRoutes = require('./routes/meals'); 
 const usersRoutes = require('./routes/users');
 const chatRoutes = require('./routes/chat');
 const weightRoutes = require('./routes/weight');
 
-app.use('/api/nutrition', nutritionRoutes);
-app.use('/api/meals', mealsRoutes);
+app.use('/api/nutrition', nutritionRoutes); 
+app.use('/api/meals', mealsRoutes); 
 app.use('/api/users', usersRoutes);
 app.use('/api/chat', chatRoutes);
-app.use('/api/weight', weightRoutes);
+app.use('/api/weight', weightRoutes); 
 
-// Create an HTTPS server and listen on the specified port
-https.createServer(sslOptions, app).listen(port, () => {
-    console.log(`Server is running securely on https://localhost:${port}`);
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
